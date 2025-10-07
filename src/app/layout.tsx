05e571d2fe6f2d2ec1dd1bca6cab1107689f0053
SoonYoung Park <bsy0302@naver.com>
2025-10-07 12:55:06 +0900

    Design: Pretendard 폰트 설정
    Pretendard 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,38 +4,41 @@ import "./globals.css";
 import AppShell from "../components/layout/AppShell";
 
 const pretendard = localFont({
-  src: [
-    {
-      path: "../../public/fonts/Pretendard-Light.otf",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Pretendard-Regular.otf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Pretendard-Medium.otf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Pretendard-SemiBold.otf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Pretendard-Bold.otf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Pretendard-ExtraBold.otf",
-      weight: "800",
-      style: "normal",
-    },
-  ],
+  // src: [
+  //   {
+  //     path: "../../public/fonts/Pretendard-Light.otf",
+  //     weight: "300",
+  //     style: "normal",
+  //   },
+  //   {
+  //     path: "../../public/fonts/Pretendard-Regular.otf",
+  //     weight: "400",
+  //     style: "normal",
+  //   },
+  //   {
+  //     path: "../../public/fonts/Pretendard-Medium.otf",
+  //     weight: "500",
+  //     style: "normal",
+  //   },
+  //   {
+  //     path: "../../public/fonts/Pretendard-SemiBold.otf",
+  //     weight: "600",
+  //     style: "normal",
+  //   },
+  //   {
+  //     path: "../../public/fonts/Pretendard-Bold.otf",
+  //     weight: "700",
+  //     style: "normal",
+  //   },
+  //   {
+  //     path: "../../public/fonts/Pretendard-ExtraBold.otf",
+  //     weight: "800",
+  //     style: "normal",
+  //   },
+  // ],
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "100 900",
   variable: "--font-pretendard",
 });
 
@@ -50,8 +53,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body className={`${pretendard.variable} font-sans`}>{children}</body>
+    <html lang="ko" className={`${pretendard.variable}`}>
+      <body className={pretendard.className}>{children}</body>
     </html>
   );
 }
